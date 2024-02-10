80c9ff4af4c7dcb6a58dd49a89cb561149148fb4
abdujalil-sobirov-fronted <abdujalil-sobirov-fronted@gmail.com>
2024-02-10 10:32:09 -0700

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,110 @@
-click_to_record.addEventListener('click',function(){
-	var speech = true;
-	window.SpeechRecognition = window.webkitSpeechRecognition;
+const recordBtn = document.querySelector(".record"),
+  result = document.querySelector(".result"),
+  downloadBtn = document.querySelector(".download"),
+  inputLanguage = document.querySelector("#language"),
+  clearBtn = document.querySelector(".clear");
 
-	const recognition = new SpeechRecognition();
-	recognition.interimResults = true;
+let SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition,
+  recognition,
+  recording = false;
 
-	recognition.addEventListener('result', e => {
-			const transcript = Array.from(e.results)
-					.map(result => result[0])
-					.map(result => result.transcript)
-					.join('')
+function populateLanguages() {
+  languages.forEach((lang) => {
+    const option = document.createElement("option");
+    option.value = lang.code;
+    option.innerHTML = lang.name;
+    inputLanguage.appendChild(option);
+  });
+}
 
-			document.getElementById("convert_text").innerHTML = transcript;
-			console.log(transcript);
-	});
-	
-	if (speech == true) {
-			recognition.start();
-	}
-})
+populateLanguages();
+
+function speechToText() {
+  try {
+    recognition = new SpeechRecognition();
+    recognition.lang = inputLanguage.value;
+    recognition.interimResults = true;
+    recordBtn.classList.add("recording");
+    recordBtn.querySelector("p").innerHTML = "Listening...";
+    recognition.start();
+    recognition.onresult = (event) => {
+      const speechResult = event.results[0][0].transcript;
+      //detect when intrim results
+      if (event.results[0].isFinal) {
+        result.innerHTML += " " + speechResult;
+        result.querySelector("p").remove();
+      } else {
+        //creative p with class interim if not already there
+        if (!document.querySelector(".interim")) {
+          const interim = document.createElement("p");
+          interim.classList.add("interim");
+          result.appendChild(interim);
+        }
+        //update the interim p with the speech result
+        document.querySelector(".interim").innerHTML = " " + speechResult;
+      }
+      downloadBtn.disabled = false;
+    };
+    recognition.onspeechend = () => {
+      speechToText();
+    };
+    recognition.onerror = (event) => {
+      stopRecording();
+      if (event.error === "no-speech") {
+        alert("No speech was detected. Stopping...");
+      } else if (event.error === "audio-capture") {
+        alert(
+          "No microphone was found. Ensure that a microphone is installed."
+        );
+      } else if (event.error === "not-allowed") {
+        alert("Permission to use microphone is blocked.");
+      } else if (event.error === "aborted") {
+        alert("Listening Stopped.");
+      } else {
+        alert("Error occurred in recognition: " + event.error);
+      }
+    };
+  } catch (error) {
+    recording = false;
+
+    console.log(error);
+  }
+}
+
+recordBtn.addEventListener("click",() => {
+  if (!recording) {
+    speechToText();
+    recording = true;
+  } else {
+    stopRecording();
+  }
+});
+
+function stopRecording() {
+  recognition.stop();
+  recordBtn.querySelector("p").innerHTML = "Start Listening";
+  recordBtn.classList.remove("recording");
+  recording = false;
+}
+
+function download() {
+  const text = result.innerText;
+  const filename = "speech.txt";
+
+  const element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+  element.style.display = "none";
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
+downloadBtn.addEventListener("click",() => {
+  result.innerHTML = "";
+  downloadBtn.disabled = true;
+});
